5852f18979bf6eb206ccbfee173e153521b4b979
radhethakur-07 <harikrdbg121@gmail.com>
2026-09-14 09:58:43 +0530

    fix: apply user provided posters and backdrops for Panchayat and Mirzapur The Movie

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -27,6 +27,11 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'i.ytimg.com',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.etimg.com',
+        pathname: '/**',
       }
     ],
   },
